b05ca36d1f39d17448616300b400b9b5791c884a
Yoshiki Kojima <kojima@goodpatch.com>
2014-08-20 01:10:10 +0900

    ＃３配列の話を追加

--- SwiftFirstPrograming2.playground/section-1.swift
+++ SwiftFirstPrograming2.playground/section-1.swift
@@ -1,6 +1,10 @@
 //はじめてのSwiftプログラミング「文字列のお話！」
 
+//文字列リテラルはダブルクオート、連結には＋演算子を使う
 var name = "Yoshiki" + "Kojima"
+
+//文字列の長さを取得する
 countElements(name)
 
+//文字列中に変数を埋め込む方法。バックスラッシュに続いて括弧で変数を囲う
 println("My name is \(name)")
---
+++ SwiftFirstPrograming3.playground/section-1.swift
@@ -0,0 +1,15 @@
+//はじめてのSwiftプログラミング「配列のお話！」
+
+//配列を作る
+var animals = ["cat","dog","cow","monkey"]
+
+//配列の要素を数える
+animals.count
+
+//配列の0番目を取得
+animals[0]
+
+//配列の中身をすべて出力
+for animal in animals {
+    println(animal)
+}
